d09b6b2e9be4e3191fbc59ccd054d6366059717f
josh <joshuaturi391@gmail.com>
2025-04-30 12:41:30 -0400

    aweosme

--- camrun.py
+++ camrun.py
@@ -75,7 +75,6 @@ from PIL import Image
 import os
 ####
 #### Directory settings
-user=os.getlogin()
 out_paren_dir='/mnt/photo-storage/' # output file path
 log_dir='./output'
 log1=log_dir+'log_ASC_control.log' # logfile that errors are recorded mainly
